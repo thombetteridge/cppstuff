3499df1e1e40ce5fa16dce6cc0f116d222ca3c76
Tom Betteridge <thombetteridge@gmail.com>
2024-08-20 20:26:53 +0930

    t

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include <vector>
+
 int main() {
     std::cout << "hello world\n";
 
+    std::vector<int> v;
+
+    for (int i = 0; i < 10; i++) {
+        v.push_back(i);
+    }
+
+    for (auto &i : v) {
+        std::cout << i << "\n";
+    }
+
     return 0;
 }
+
