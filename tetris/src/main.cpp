4a2cacc17b555fb5608ab0e209a61740d04fa8ec
tomB <thombetteridge@gmail.com>
2024-08-24 17:03:07 +0930

    nothing

--- tetris/src/main.cpp
+++ tetris/src/main.cpp
@@ -2,9 +2,7 @@
 #include "game.hpp"
 #include <iostream>
 
-double last_update_time = 0;
-
-auto event_triggered(double interval) -> bool
+auto event_triggered(double interval, double& last_update_time) -> bool
 {
   double current_time = GetTime();
   if (current_time - last_update_time >= interval)
@@ -17,41 +15,34 @@ auto event_triggered(double interval) -> bool
 
 auto main() -> int
 {
-  // Initialization
   const int screenWidth  = 300;
   const int screenHeight = 600;
 
   const double game_speed = 0.2;
 
-  raylib::Color  textColor(LIGHTGRAY);
-  raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
+  double last_update_time = 0;
+
+  raylib::Window w(screenWidth, screenHeight, "Tetris");
 
   SetTargetFPS(60);
 
   auto game = Game();
-
-  std::cout << "hello";
 
   // Main game loop
   while (!w.ShouldClose())  // Detect window close button or ESC key
   {
     // Update
     game.handleInput();
-    if (event_triggered(game_speed))
+    if (event_triggered(game_speed, last_update_time))
     {
       game.moveBlockDown();
     }
 
-    // TODO: Update your variables here
-
     // Draw
     BeginDrawing();
     ClearBackground(DARKGRAY);
-
     game.draw();
-
     EndDrawing();
   }
-
   return 0;
 }
